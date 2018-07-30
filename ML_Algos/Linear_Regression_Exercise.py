205af5261630e45f3cf2aa0c203eae1ed582b0f4
Pankaj Sanwal <sanwal.pankaj@gmail.com>
2018-07-30 20:34:44 +0530

    nothing

--- ML_Algos/Linear_Regression_Exercise.py
+++ ML_Algos/Linear_Regression_Exercise.py
@@ -1,6 +1,14 @@
+#%%
+# Importing useful libs
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn import metrics 
+from sklearn import metrics
+
+#%%
+# Reading Data
+df = pd.read_csv(r'C:\Users\pankaj.sanwal\Desktop\PythonStuff\Python-Data-Science-and-Machine-Learning-Bootcamp\Python-Data-Science-and-Machine-Learning-Bootcamp\Machine Learning Sections\Linear-Regression\Ecommerce Customers')
+df.head()
+df.columns
